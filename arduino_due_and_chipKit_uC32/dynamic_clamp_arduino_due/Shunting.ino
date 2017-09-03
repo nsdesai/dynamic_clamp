03829273ef1b40fff0e9f093fd25794728173d12
Niraj Desai <desai@utexas.edu>
2017-09-03 18:04:47 -0500

    Add files via upload

---
+++ arduino_due_and_chipKit_uC32/dynamic_clamp_arduino_due/Shunting.ino
@@ -0,0 +1,7 @@
+// A constant conductance with a reversal potential of -70 mV
+float Shunting(float v) {
+  float current = -gShunt * (v + 70);
+  return current;
+}
+
+
---
+++ arduino_due_and_chipKit_uC32/dynamic_clamp_chipKit_uC32/Sodium.ino
@@ -0,0 +1,40 @@
+// A fast, transient Na+ conductance using the Hodgkin-Huxley formalism.
+// For speed, the parameters alphaM, betaM, alphaH, and betaH are pre-calculated
+// and put in lookup tables stored as global variables.
+
+// Declare the lookup table variables
+float alphaM[151] = {0.0};                          // Pre-calculate activation and inactivation parameters 
+float betaM[151] = {0.0};                           // for sodium currents: Vm from -100 mV to +50 mV in 
+float alphaH[151] = {0.0};                          // steps of 1 mV
+float betaH[151] = {0.0};
+
+// Generate the lookup tables
+void GenerateSodiumLUT() {
+  float v;
+  for (int x=0; x<151; x++) {
+    v = (float)x - 100.0;                          // We use membrane potentials between -100 mV and +50 mV
+    if (x==60) {                                   // The if-else statement makes sure alphaM stays finite at v = -40 mV.
+      alphaM[x] = 1.0;
+    } else {
+      alphaM[x] = 0.1 * (-(v+40.0)) / ( exp( -(v+40.0)/10 ) - 1 );
+    }
+    betaM[x] = 4.0 * exp(-(v+65)/18);
+    alphaH[x] = 0.07 * exp(-(v+65)/20);
+    betaH[x] = 1 / ( exp(-(v+35)/10 ) + 1); 
+  }
+}
+
+// At every time step, calculate the sodium current in the Hodgkin-Huxley manner
+float Sodium(float v) {
+  static float mNaVar = 0.0;    // activation gate
+  static float hNaVar = 1.0;    // inactivation gate
+  int vIdx = (int)v + 100;
+  vIdx = constrain(vIdx,0,150);
+  mNaVar = mNaVar + dt * ( alphaM[vIdx]*(1-mNaVar) - betaM[vIdx]*mNaVar );
+  if (mNaVar < 0.0) mNaVar = 0.0;
+  hNaVar = hNaVar + dt * ( alphaH[vIdx]*(1-hNaVar) - betaH[vIdx]*hNaVar );
+  if (hNaVar < 0.0) hNaVar = 0.0;
+  float current1 = -gNa * mNaVar * mNaVar * mNaVar * hNaVar * (v - 50);  // ENa = +50 mV
+  return current1;
+}
+
